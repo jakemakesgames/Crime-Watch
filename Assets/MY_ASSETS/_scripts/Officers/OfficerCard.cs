26eb3d0c288cd63478f3863723eba918fe278404
jakemakesgames <jakebrown.designer@gmail.com>
2018-02-19 17:03:43 +1100

    added UI, dialogue system and officer card prefab. playable scene and menu created.

---
+++ Assets/MY_ASSETS/_scripts/Officers/OfficerCard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New Officer", menuName = "Officer")]
+public class OfficerCard : ScriptableObject 
+{
+	// name and description of officer //
+	public new string name;
+	public string description;
+
+	// image of officer //
+	public Sprite image;
+
+	// resources and currency costs //
+	public int moneyCost;
+	public int level;
+	public int xp;
+
+	public void Print()
+	{
+		Debug.Log (name + ": " + description + ", " + moneyCost + ", " + level + ", " + xp);
+	}
+
+}
